d1efffc64704abc6aa8422a4219970147cd9783e
Sumit Kharche <sumit.kharche@yahoo.in>
2021-11-14 17:15:44 +0530

    updated header component to fix error

--- components/partials/header.js
+++ components/partials/header.js
@@ -3,15 +3,14 @@ import React from 'react';
 export default class extends React.Component {
   render() {
     return [
-      <Head key={1}>
-        <link rel="stylesheet" type="text/css" href="/static/style.css"/>
-        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet"/>
-        <link href="https://fonts.googleapis.com/css?family=Droid+Serif" rel="stylesheet"/>
-        <meta charset="UTF-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
-        <title>{ this.props.cosmic.post ? this.props.cosmic.post.title + ' |' : '' } Simple React Blog</title>
+      <Head>
+        <link rel="stylesheet" type="text/css" href="/static/style.css" key="cssfile" />
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" key="fontfile"/>
+        <link href="https://fonts.googleapis.com/css?family=Droid+Serif" rel="stylesheet" key="anotherfontfile"/>
+        <meta name="viewport" content="width=device-width" key="metaviewp"/>
+        <title key="sitetitle">{ this.props.cosmic.post ? this.props.cosmic.post.title + ' |' : '' } Simple React Blog</title>
       </Head>,
-      <header className="header" key={2}>
+      <header className="header" key="headerelement">
         <h1 className="site-title">
           <a href="/">{ this.props.cosmic.global.header.metadata.site_title }</a>
           <small className="site-title__tag">{ this.props.cosmic.global.header.metadata.site_tag }</small>
