fcbd7311420055d8eedcaa391ce5827222c58600
Karl Koch <karl@kejk.tech>
2023-05-29 21:19:29 +0100

    fix: tag text size

--- components/Tag.jsx
+++ components/Tag.jsx
@@ -1,7 +1,7 @@
-export default function Tag({children}) {
+export default function Tag({ children }) {
   return (
-    <div className="flex bg-zinc-50 dark:bg-zinc-900 border border-zinc-200 dark:border-zinc-700 justify-center px-2 py-1 rounded-full text-cyan-600 dark:text-cyan-300">
+    <div className='flex bg-zinc-50 dark:bg-zinc-900 border border-zinc-200 dark:border-zinc-700 justify-center px-2 py-1 rounded-full text-cyan-600 dark:text-cyan-300 text-xs md:text-sm lg:text-base'>
       {children}
-    </div> 
-  )
+    </div>
+  );
 }
