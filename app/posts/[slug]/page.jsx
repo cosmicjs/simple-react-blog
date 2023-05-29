0c8efff720c64569e46d8701e6bdb40272562280
Karl Koch <karl@kejk.tech>
2023-05-29 21:22:25 +0100

    fix: mobile layout of post page

--- app/posts/[slug]/page.jsx
+++ app/posts/[slug]/page.jsx
@@ -48,13 +48,13 @@ export default async ({ params }) => {
       {post && post.metadata.hero.imgix_url && (
         <Image className='w-full h-[640px] object-cover bg-no-repeat object-center mb-5' src={`${post.metadata.hero.imgix_url}?w=1400&auto=format`} width={2000} height={640} />
       )}
-      <main className='w-full flex justify-start px-4 items-start'>
-        <div className='flex mt-4 w-1/4 justify-center'>
+      <main className='w-full flex flex-col md:flex-row justify-start px-4 items-start'>
+        <div className='flex pb-4 md:pb-0 mt-4 w-1/4 justify-start md:justify-center'>
           <Link href='/' className='p-2 rounded-full bg-white dark:bg-zinc-950 shadow-md border-zinc-100 dark:border-zinc-800 text-zinc-700 dark:text-zinc-300'>
             <ArrowLeft className='h-4 w-4' />
           </Link>
         </div>
-        <div className='flex flex-col w-2/4'>
+        <div className='flex flex-col w-full md:w-2/4'>
           <h2>
             {!post && <div className='text-center'>Post Not found</div>}
             {post && <Link href={`/posts/${post.slug}`}>{post.title}</Link>}
